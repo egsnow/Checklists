6594934b22a5ca22793ed9eba1521a5b47fff0f5
Eric Snow <ericsnow@Kristinas-MBP.attlocal.net>
2018-10-19 15:20:41 -0500

    Checklist Homework chapter 9

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Eric Snow on 10/19/18.
+//  Copyright © 2018 Eric Snow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
