ee1eebabcab1fe922c7dc9ac884f6353bec9f45d
Eric Snow <ericsnow@Kristinas-MBP.attlocal.net>
2018-10-28 13:36:52 -0500

    homework 10/28/18

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  Checklists
+//
+//  Created by Eric Snow on 10/28/18.
+//  Copyright © 2018 Eric Snow. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    
+    var text = ""
+    var checked = false
+    
+    
+    func toggleChecked() {
+        checked = !checked
+    }
+    
+}
+
