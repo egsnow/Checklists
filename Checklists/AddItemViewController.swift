cf9d703ff8f74e215d682b2c78dd026c068bff8e
Eric Snow <ericsnow@Kristinas-MBP.attlocal.net>
2018-11-11 16:59:54 -0600

    Added textfield to add item screen. Done button ends on exit feature added.

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,58 @@
+//
+//  AddItemTableViewController.swift
+//  Checklists
+//
+//  Created by Eric Snow on 11/4/18.
+//  Copyright © 2018 Eric Snow. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    // MARK:- Actions
+    @IBAction func cancel() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK:- Table View Delegates
+    override func tableView(_ tableView: UITableView,
+                            willSelectRowAt indexPath: IndexPath)
+        -> IndexPath? {
+            return nil
+    }
+    
+    // MARK:- Text Field Delegates
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange,
+                                                  with: string)
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
+    }
+}
